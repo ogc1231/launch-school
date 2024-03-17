2d879d28727fd01c4f110d9d0bc7f57d4e361d0c
EC2 Default User <ec2-user@ip-172-31-4-62.eu-west-1.compute.internal>
2024-03-17 11:01:30 +0000

    Finish reading documentatiom 1 questions

---
+++ JS100/js_basics_questions/reading-docs-1.js
@@ -0,0 +1,41 @@
+// 1
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// 2
+// .toLowerCase()
+
+// 3
+// no built in method to capitalise first letter of string
+
+// 4
+// ['fish', 'and', 'chips'][1]
+
+// 5
+// undefined
+
+// 6
+// 'palm tree'
+// 'palm tree'
+// 'sequoia'
+
+// 7 
+// number
+// string
+// boolean
+// number
+// object
+// undefined
+
+// 8
+// string
+// object
+// boolean
+
+// 9
+// ["Starting", "to", "get", "the", "hang", "of", "it...", "#javascript", "#launchschool"]
+// ["#launchschool", "#javascript", "it...", "of", "hang", "the", "get", "to", "Starting"]
+// '#launchschool #javascript it... of hang the get to Starting'
+
+// 10
+// true
+// false
