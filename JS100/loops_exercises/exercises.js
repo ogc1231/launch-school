37ba4ff9627bcb4765acf93d5f7bcca0bcb14315
EC2 Default User <ec2-user@ip-172-31-4-62.eu-west-1.compute.internal>
2024-03-19 19:18:47 +0000

    Finish loops exercises

--- JS100/loops_exercises/exercises.js
+++ JS100/loops_exercises/exercises.js
@@ -1,15 +1,70 @@
 // 1
-let rlSync = require('readline-sync');
+// let rlSync = require('readline-sync');
 
-let age = Number(rlSync.question(`How old are you? `));
+// let age = Number(rlSync.question(`How old are you? `));
 
-console.log(`You are ${age} years old.`);
+// console.log(`You are ${age} years old.`);
 
-for(let i = 10; i <= 70; i +=10){
-    console.log(`In ${i} years, you will be ${age + i} years old.`)
+// for(let i = 10; i <= 70; i +=10){
+//     console.log(`In ${i} years, you will be ${age + i} years old.`)
+// }
+
+// 2
+// function factorial(number) {
+//     let result = 1;
+//     for(let  i = number; i > 0; i -= 1) {
+//         result *= i;
+//     }
+//     return result
+// }
+// console.log(factorial(1));  
+// console.log(factorial(2));   
+// console.log(factorial(3));    
+// console.log(factorial(4));    
+// console.log(factorial(5));    
+// console.log(factorial(6));    
+// console.log(factorial(7));    
+// console.log(factorial(8));
+// console.log(factorial(9));
+
+// 3 
+// counter keeps being reasigned to one and never reaching the break
+
+// 4
+// no because the increment is still added to the i value but inside the console log
+
+// 5 
+function randomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// console.log(`In 10 years, you will be ${age + 10} years old.`);
-// console.log(`In 20 years, you will be ${age + 20} years old.`);
-// console.log(`In 30 years, you will be ${age + 30} years old.`);
-// console.log(`In 40 years, you will be ${age + 40} years old.`);
+let tries = 0;
+let result;
+
+ do {
+  result = randomNumberBetween(1, 6);
+  tries += 1;
+} while (result <= 2);
+
+console.log('It took ' + String(tries) + ' tries to get a number greater than 2');
+
+// 6
+function factorial(num) {
+    if (num === 1) {
+        return 1
+    }
+    
+    return num * factorial(num - 1)
+}
+
+
+console.log(factorial(1));  
+console.log(factorial(2));   
+console.log(factorial(3));    
+console.log(factorial(4));    
+console.log(factorial(5));    
+console.log(factorial(6));    
+console.log(factorial(7));    
+console.log(factorial(8));
+console.log(factorial(9));
+  
