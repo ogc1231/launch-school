b6338844f55353e291a34132d7be22185d1cab58
Oliver Craigie <ogc@live.ie>
2024-03-21 16:21:29 +0000

    Completed qustions 8 & 9

--- JS100/objects_exercises/exercises.js
+++ JS100/objects_exercises/exercises.js
@@ -87,3 +87,33 @@
 // }
 
 // 8
+// let objToCopy = {
+//   foo: 1,
+//   bar: 2,
+//   qux: 3,
+// };
+
+// function copyObj(sourceObject, keys) {
+//   let newObject = {}
+  
+//   if (keys) {
+//     keys.forEach((key) => {
+//       newObject[key] = sourceObject[key]
+//     })
+//     return newObject
+//   } 
+//   else {
+//     return Object.assign(newObject, sourceObject)
+//   }
+// }
+// let newObj = copyObj(objToCopy);
+// console.log(newObj);        // => { foo: 1, bar: 2, qux: 3 }
+
+// let newObj2 = copyObj(objToCopy, [ 'foo', 'qux' ]);
+// console.log(newObj2);       // => { foo: 1, qux: 3 }
+
+// let newObj3 = copyObj(objToCopy, [ 'bar' ]);
+// console.log(newObj3);       // => { bar: 2 }
+
+// 9
+// 'hi' and 'hello'
