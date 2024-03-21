22dfd7a25e8f985b7c469b3f7c66f2f8d01c6ce6
Oliver Craigie <ogc@live.ie>
2024-03-21 10:17:29 +0000

    Finish array exercises

--- JS100/arrays_exercises/exercises.js
+++ JS100/arrays_exercises/exercises.js
@@ -46,21 +46,94 @@
 // })
 
 // 4
-let myArray3 = [
-  1, 3, 6, 11,
-  4, 2, 4, 9,
-  17, 16, 0,
-];
+// let myArray3 = [
+//   1, 3, 6, 11,
+//   4, 2, 4, 9,
+//   17, 16, 0,
+// ];
 
-let newArray = myArray3.map(function(value) {
-    if(value % 2 === 0) {
-        return "even"
-    }
-    else {
-        return "odd"
-    }
-})
+// let newArray = myArray3.map(function(value) {
+//     if(value % 2 === 0) {
+//         return "even"
+//     }
+//     else {
+//         return "odd"
+//     }
+// })
 
-console.log(newArray)
+// console.log(newArray)
 
 // 5
+// let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+
+// function findIntegers(array) {
+//     return array.filter(function(element) {
+//         return Number.isInteger(element);
+//     })
+// }
+
+// let integers = findIntegers(things);
+// console.log(integers);
+
+// 6
+
+// let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+
+// function oddLengths(arr) {
+//     let length = arr.map((value) => value.length)
+//     return length.filter(function(value2) {
+//         if (value2 % 2 !== 0) {
+//             return value2
+//         }
+//     })
+// }
+
+// console.log(oddLengths(arr));
+
+// 7
+// let array = [3, 5, 7];
+
+// function sumOfSquares(array) {
+//   return array.reduce((sum, value) => {
+//       return sum + value * value
+//     }, 0)
+// }
+
+// console.log(sumOfSquares(array)); // => 83
+
+// 8
+
+// let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+
+// function oddLengths(arr) {
+    
+//     return arr.reduce((sum, value) => {
+//         let value2 = value.length
+//         if (value2 % 2 !== 0) {
+//             sum.push(value2)
+//         }
+//         return sum
+//     }, [])
+// }
+
+// console.log(oddLengths(arr)); // => [1, 5, 3]
+
+// 9
+// let numbers1 = [1, 3, 5, 7, 9, 11];
+// let numbers2 = [];
+// let numbers3 = [2, 4, 6, 8];
+
+// console.log(numbers1.includes(3))
+// console.log(numbers2.includes(3))
+// console.log(numbers3.includes(3))
+
+// 10
+let arr = [
+  ["hello", "world"],
+  ["example", "mem", null, 6, 88],
+  [4, 8, 12]
+];
+
+arr[1][3] = 606
+
+console.log(arr)
