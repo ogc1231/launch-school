bf9ea0a263a83397e6019cdbb30aa2f8582133c2
Oliver Craigie <ogc@live.ie>
2024-03-22 17:38:30 +0000

    Finish more exercises

--- JS100/more_exercises/exercises.js
+++ JS100/more_exercises/exercises.js
@@ -22,3 +22,45 @@
 // console.log(doSomething("Pursuit of happiness"));
 
 // 6
+// let words = [
+//   'laboratory',
+//   'experiment',
+//   'flab',
+//   'Pans Labyrinth',
+//   'elaborate',
+//   'polar bear',
+// ];
+
+// function allMatches(words, pattern) {
+//     return words.filter((word) => pattern.test(word))
+// }
+
+// console.log(allMatches(words, /la/));
+
+// 7
+// Allows code to run when a error occurs by catching errors  in a manageable and predictable manner and return a messgae
+
+// 8
+// function isNotANumber(value) {
+//   return value !== value
+// }
+
+// console.log(isNotANumber(1))
+// console.log(isNotANumber(NaN))
+
+// 9
+// function zero(value) {
+//     return 1 / value === -Infinity;
+// }
+
+// console.log(zero(-0))
+
+// 10
+let x = "5"
+x = x + 1
+
+let y = "5"
+y++
+
+console.log(x)
+console.log(y)
