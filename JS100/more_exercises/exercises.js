6bcfc5ad160228afff4ca37af771719edd269ec3
Oliver Craigie <ogc@live.ie>
2024-03-22 11:55:40 +0000

    Finish questions 1-5

---
+++ JS100/more_exercises/exercises.js
@@ -0,0 +1,24 @@
+// 1
+// [1, 4, 3]
+
+// 2
+// greeting is not defined
+
+// 3
+// console.log(Math.sqrt(37))
+
+// 4
+// console.log(Math.max(1, 6, 3, 2))
+// console.log(Math.max(-1, -6, -3, -2))
+// console.log(Math.max(2, 2))
+
+// 5
+// splits string into an array when there is any empty space, reverses the array and returns the length of strings in the array
+
+// function doSomething(string) {
+//   return string.split(' ').reverse().map((value) => value.length);
+// }
+
+// console.log(doSomething("Pursuit of happiness"));
+
+// 6
