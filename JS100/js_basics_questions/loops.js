d72823c4111218fc68890db6003aa57223fc67ad
Oliver Craigie <ogc@live.ie>
2024-03-23 12:27:35 +0000

    Finish loop questions

---
+++ JS100/js_basics_questions/loops.js
@@ -0,0 +1,94 @@
+// 1
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i)
+// }
+
+// 2
+// for (let i = 10; i > 0; i -= 1) {
+//   console.log(i);
+// }
+// console.log('Launch!');
+
+// 3
+// let greeting = 'Aloha!';
+
+// for(let i = 0; i < 3; i += 1){
+//     console.log(greeting)
+// }
+
+
+// let greeting = 'Aloha!'
+// let count = 0
+
+// while (count < 3) {
+//     console.log(greeting)
+//     count += 1
+// }
+
+// 4
+// for(let i = 1; i <= 100; i += 1) {
+//     console.log(i * 2)
+// }
+
+// 5
+// let array = [1, 2, 3, 4];
+// let index = 0;
+
+// while (index < array.length ) {
+//     console.log(array[index])
+//     index += 1
+// }
+
+// 6
+// let cities = ['Istanbul', 'Los Angeles', 'Tokyo', null, 'Vienna', null, 'London', 'Beijing', null];
+
+// for (let i = 0; i < cities.length; i += 1) {
+//     if(cities[i] === null) {
+//         continue;
+//     }
+//     console.log(cities[i].length)
+// }
+
+// 7
+// no upper limit condition for when to stop running loop
+// for (let i = 0; i < 1; i += 1) {
+//   console.log("and on");
+// }
+
+// for (let i = 0; ; i += 1) {
+//   console.log("and on");
+//   break;
+// }
+
+// 8
+// let n = 1;
+
+// while (n < 40) {
+//     console.log(n)
+//     n += 2
+// }
+
+// let number = 1
+
+// while (number < 40) {
+//     if (number % 2 === 1) {
+//         console.log(number)
+//     }
+//     number += 1
+// }
+
+// 9
+// let fish = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce'];
+
+// for (let i = 0; i < fish.length; i += 1) {
+//     console.log(fish[i]);
+    
+//     if (fish[i] === 'Nemo') {
+//         break;
+//     }
+// }
+
+// 10
+// while loop evaluates condition before code block is executed, 
+// do-while loop evaluates condition after code block is executed
+// do-while always executed at least once
---
+++ JS100/js_basics_questions/strings.js
@@ -0,0 +1,9 @@
+// 1
+// let string = "These aren't the droids you're looking for."
+// console.log(string.length)
+
+// 2
+// let string = 'confetti floating everywhere'
+// console.log(string.toUpperCase())
+
+// 3
