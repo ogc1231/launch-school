09476a0b6c4e7e0ae40c288f40d3faf4dab493e2
Oliver Craigie <ogc@live.ie>
2024-03-23 10:30:46 +0000

    Finish documentation 2 questions

--- JS100/js_basics_questions/reading-docs-2.js
+++ JS100/js_basics_questions/reading-docs-2.js
@@ -1,10 +1,59 @@
 // 1
-let iceCreamTaste = 'chocolate';
-let iceCreamDensity = 10;
+// let iceCreamTaste = 'chocolate';
+// let iceCreamDensity = 10;
 
-while(iceCreamDensity > 0) {
-    console.log('Drip...');
-    iceCreamDensity -= 1;
-}
+// while(iceCreamDensity > 0) {
+//     console.log('Drip...');
+//     iceCreamDensity -= 1;
+// }
 
-console.log(`The ${iceCreamTaste} ice cream melted.`);
+// console.log(`The ${iceCreamTaste} ice cream melted.`);
+
+// 2
+// String, Number, Boolean, null, Undefined, BigInt, Symbol
+
+// 3
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// 4
+// 4 * 5 + 3 ** 2 / 10
+// 20 + 9 / 10
+// 20 + 0.9
+// 20.9
+
+// 5
+// returns 1711188435194, timestamp of current time in milliseconds
+
+// 6
+// 124
+// 2024
+// should use full year as gives exact year and getYear is deprecated
+
+// 7
+// one agrument a seperater point such as a comma or space
+// default to comma as joing point if no argument
+// extra arguments are ignored
+
+// 8
+// 1 - Array.prototype.concat()
+// 2 - "string" + "other string"
+
+// 9
+// let speedLimit = 60;
+// let currentSpeed = 80;
+
+// if ((currentSpeed > speedLimit) && ((currentSpeed - speedLimit) > 5)) {
+//   console.log('"People are so bad at driving cars ' +
+//     'that computers don\'t have to be that good to be much better." ' +
+//     '-- Marc Andreessen');
+// }
+// missing paranthesis in if statement
+
+// 10
+// let tweet = 'Woohoo! :-)';
+
+// if (tweet.length > 140) {
+//   console.log('Tweet is too long!');
+// }
+// Don't need to call length() as function, just .length
