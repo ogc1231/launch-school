cd86dd5f17658deb43f3cdc1f2dd7318cfc9f779
Oliver Craigie <ogc@live.ie>
2024-03-24 17:40:57 +0000

    Finish conditionals questions

--- JS100/js_basics_questions/conditionals.js
+++ JS100/js_basics_questions/conditionals.js
@@ -1,1 +1,63 @@
 // 1
+// false, null, undefined, NaN, 0, ""
+
+// 2
+// let randomNumber = Math.round(Math.random());
+
+// if (randomNumber === 1) {
+//     console.log('Yes!')
+// }
+// else {
+//     console.log('No.')
+// }
+
+// 3
+// let randomNumber = Math.round(Math.random());
+// randomNumber ? console.log('Yes!') : console.log('No.')
+
+// 4
+// let weather = 'snowy'
+
+// if (weather === 'sunny') {
+//     console.log("It's a beautiful day!")
+// }
+// else if (weather === 'rainy') {
+//     console.log("Grab your umbrella.")
+// }
+// else {
+//     console.log("Let's stay inside.")
+// }
+
+// 5
+// 'neigh'
+// 'tweet tweet'
+// '*cricket*'
+
+// 6
+// let weather = 'rainy'
+
+// switch(weather) {
+//     case 'sunny':
+//         console.log("It's a beautiful day!");
+//         break;
+//     case 'rainy':
+//         console.log("Grab your umbrella");
+//         break;
+//     default:
+//       console.log("Let's stay inside.");
+//
+
+// 7
+// 'Yes!'
+
+// 8
+// 'No...'
+
+// 9
+// $3.99
+
+// 10
+// 19 < 24 && (0 > 0 || 24 > 0)
+// 19 < 24 && true
+// true && true
+// true
