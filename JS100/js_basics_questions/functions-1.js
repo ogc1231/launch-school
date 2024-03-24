6c4675897e2a6a48ad03ef6c3add233bc083f2f0
Oliver Craigie <ogc@live.ie>
2024-03-24 17:53:00 +0000

    Finish functions questions 1-5

---
+++ JS100/js_basics_questions/functions-1.js
@@ -0,0 +1,32 @@
+// 1
+// function sum(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(sum(22, 10));
+
+// 2
+// function brendanEichQuote() {
+//     console.log('Always bet on JavaScript.')
+// }
+
+// brendanEichQuote()
+
+// 3
+// function cite(string1, string2) {
+//     console.log(`${string1} said: "${string2}"`)
+// }
+
+// cite('Brendan Eich', 'Always bet on JavaScript.');
+
+// 4
+// function squaredNumber(num) {
+//     return num ** 2;
+// }
+
+// console.log(squaredNumber(3));
+
+// 5
+// nothing, function not called/invoked
+
+// 6
