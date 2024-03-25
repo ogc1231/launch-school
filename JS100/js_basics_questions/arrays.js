ca2c41489c730f071972b109412d03f4980d9975
Oliver Craigie <ogc@live.ie>
2024-03-25 11:44:55 +0000

    Finish questions 1-5 arrays

---
+++ JS100/js_basics_questions/arrays.js
@@ -0,0 +1,29 @@
+// 1
+// function first(array) {
+//     return array[0]
+// }
+// console.log(first(['Earth', 'Moon', 'Mars']));
+// undefined
+
+// 2
+// function last(array) {
+//     return array[array.length - 1]
+// }
+
+// console.log(last(['Earth', 'Moon', 'Mars']));
+
+// 3
+// let energy = ['fossil', 'solar', 'wind', 'tidal', 'fusion'];
+// energy.shift()
+// energy.push('geothermal')
+
+// console.log(energy)
+
+// 4
+// let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+// console.log(alphabet.split(''))
+// console.log(Array.from(alphabet))
+
+// 5
+let scores = [96, 47, 113, 89, 100, 102];
