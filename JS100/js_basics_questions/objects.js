d474909c82d5339112e6137c3a4b337680c777bd
Oliver Craigie <ogc@live.ie>
2024-03-25 19:13:17 +0000

    Finish objects questions 1-5

---
+++ JS100/js_basics_questions/objects.js
@@ -0,0 +1,63 @@
+// 1
+// let student = {
+//   name: 'Carmen',
+//   age: 14,
+//   grade: 10,
+//   courses: ['biology', 'algebra', 'composition', 'ceramics'],
+//   gpa: 3.75,
+// };
+
+// console.log(student.courses)
+// console.log(student['courses'])
+// undefined
+
+// 2
+// let jane = {
+//   firstName: 'Jane',
+//   lastName: 'Harrelson',
+//   age: 32,
+//   location: {
+//     country: 'Denmark',
+//     city: 'Aarhus'
+//   },
+//   occupation: 'engineer',
+// };
+
+// console.log(jane.location.country)
+// console.log(jane['location']['country'])
+
+// 3
+// let fido = {
+//   name: 'Fido',
+//   species: 'Labrador Retriever',
+//   color: 'brown',
+//   weight: 16,
+// };
+
+// // Add property 'age'.
+// // Add property 'favorite food'.
+// fido.age = 2
+// fido['favorite food'] = 'pasta'
+
+// console.log(fido)
+
+// 4
+// let jane = {
+//   firstName: 'Jane',
+//   lastName: 'Harrelson',
+//   age: 32,
+//   location: {
+//     country: 'Denmark',
+//     city: 'Aarhus'
+//   },
+//   occupation: 'engineer',
+//   greet: function(name) {
+//       console.log(`Hej, ${name}!`);
+//   }
+// };
+
+// jane.greet('Bobby'); // Hej, Bobby!
+
+// 5
+// { prefix: 'Pacific' }
+// { Indian: 'Pacific' }
