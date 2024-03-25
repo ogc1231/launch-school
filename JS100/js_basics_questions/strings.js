5b0fbaeedce83ec3445efba168639bc161b5f87c
Oliver Craigie <ogc@live.ie>
2024-03-25 11:24:17 +0000

    Finish strings questions

--- JS100/js_basics_questions/strings.js
+++ JS100/js_basics_questions/strings.js
@@ -1,9 +1,62 @@
 // 1
-// let string = "These aren't the droids you're looking for."
-// console.log(string.length)
+// let string = "These aren't the droids you're looking for.";
+// console.log(string.length);
 
 // 2
-// let string = 'confetti floating everywhere'
-// console.log(string.toUpperCase())
+// let string = 'confetti floating everywhere';
+// console.log(string.toUpperCase());
 
 // 3
+// function repeat(num, string) {
+//     let reps = ''
+    
+//     while (num > 0) {
+//         reps += string
+//         num -= 1
+//     }
+//     return reps
+// }
+// console.log(repeat(10, 'ha'));
+
+// 4
+// let string = 'A pirate I was meant to be!\nTrim the sails and roam the sea!'
+// console.log(string)
+
+// 5
+// let string1 = 'Polar Bear';
+// let string2 = 'Polar bear';
+// let string3 = 'Penguin';
+
+// console.log(string1.toUpperCase() === string2.toUpperCase())
+// console.log(string1.toUpperCase() === string3.toUpperCase())
+
+// 6
+// let byteSequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu';
+// console.log(byteSequence.includes('x'))
+
+// 7
+// function isBlank(string) {
+//     console.log(string.length === 0 ? true : false)
+// }
+
+// isBlank('mars');
+// isBlank('  ');
+// isBlank('');
+
+// 8
+// function isBlank(string) {
+//     console.log(string.trim().length === 0)
+// }
+
+// isBlank('mars');
+// isBlank('  '); 
+// isBlank('');
+
+// 9
+// let string = 'launch school tech & talk'
+// let array = string.split(' ')
+
+// let arrayCapitalised = array.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+
+// let stringNew = arrayCapitalised.join(' ')
+// console.log(stringNew)
