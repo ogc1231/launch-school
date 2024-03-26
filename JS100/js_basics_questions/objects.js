b8db53101781974b631c279db61310e3a833ab48
Oliver Craigie <ogc@live.ie>
2024-03-26 09:46:00 +0000

    Finish objects questions

--- JS100/js_basics_questions/objects.js
+++ JS100/js_basics_questions/objects.js
@@ -111,3 +111,25 @@
 
 
 // 10
+// function clone(obj) {
+//   return Object.assign({}, obj)
+// }
+
+// let person = {
+//   title: 'Duke',
+//   name: {
+//     value: 'Nukem',
+//     isStageName: true
+//   },
+//   age: 33
+// };
+
+// let clonedPerson = clone(person);
+// person.age = 34;
+
+// console.log(person.age);      
+// console.log(clonedPerson.age); 
+
+// person.name.isStageName = false;
+// console.log(person.name.isStageName);   
+// console.log(clonedPerson.name.isStageName); 
