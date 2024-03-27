e7d88f80c6cd6647c65c1a7f3e7e05a76fc3cac1
Oliver Craigie <ogc@live.ie>
2024-03-27 11:10:08 +0000

    Finish prepartions quiz

---
+++ JS101/preparations/quiz.js
@@ -0,0 +1,33 @@
+// 1
+// C - Git is a distributed version control system which was designed to be used locally without a main central repository. GitHub is a cloud-based remote repository which acts as a centralized repository for Git to enable collaboration.
+
+// 2
+// C - You should not nest Git repositories.
+
+// 3
+// A - Take notes.
+// B - Eliminate external distractions.
+// C - Look up concepts you don't understand.
+// D - Review your notes to make sure you captured the key concepts.
+
+// 4
+// B - A question that you can easily test.
+
+// 5
+// A - Undefined
+// D - Null
+// E = String
+
+// 6
+// A - undefined is a primitive value.
+// B - You can't mutate a primitive value after creating it.
+// D - NaN is a primitive value. (becasue it's a number)
+
+// 7
+// C - The code snippet contains both a statement and an expression.
+
+// 8
+// C - This code works. Once you create a variable with the let statement, you can reassign the variable to a different value.
+
+// 9
+// A - When comparing a number and a string, JavaScript coerces the string to a number.
