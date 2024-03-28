dd131e10739c6ce985ffb448ce6628f0463e00e4
Oliver Craigie <ogc@live.ie>
2024-03-28 12:11:27 +0000

    Finish calculator v2

--- JS101/lesson_2_small_programs/calculator.js
+++ JS101/lesson_2_small_programs/calculator.js
@@ -4,39 +4,39 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
-// const readline = require("readline-sync");
+const readline = require("readline-sync");
 
-// console.log("Welcome to Calculator!");
+console.log("Welcome to Calculator!");
 
-// console.log("What's the first number?");
-// let num1 = Number(readline.question());
+console.log("What's the first number?");
+let num1 = Number(readline.question());
 
-// console.log("What's the second number?");
-// let num2 = Number(readline.question());
+console.log("What's the second number?");
+let num2 = Number(readline.question());
 
-// console.log(`${num1} ${num2}`);
+console.log(`${num1} ${num2}`);
 
-// console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide 5) Power 6) Remainder");
-// let operation = readline.question();
+console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide 5) Power 6) Remainder");
+let operation = readline.question();
 
-// let output;
-// if (operation === '1') {
-//     output = num1 + num2
-// }
-// else if (operation === '2') {
-//     output = num1 - num2
-// }
-// else if (operation === '3') {
-//     output = num1 * num2
-// }
-// else if (operation === '4') {
-//     output = num1 / num2
-// }
-// else if (operation === '5') {
-//     output = num1 ** num2
-// }
-// else if (operation === '6') {
-//     output = num1 % num2
-// }
+let output;
+if (operation === '1') {
+    output = num1 + num2
+}
+else if (operation === '2') {
+    output = num1 - num2
+}
+else if (operation === '3') {
+    output = num1 * num2
+}
+else if (operation === '4') {
+    output = num1 / num2
+}
+else if (operation === '5') {
+    output = num1 ** num2
+}
+else if (operation === '6') {
+    output = num1 % num2
+}
 
-// console.log(`The result is: ${output}`);
+console.log(`The result is: ${output}`);
