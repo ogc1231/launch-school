9859dff7ffd4ae258178ea1c445a908e02decbf2
Oliver Craigie <ogc@live.ie>
2024-03-31 09:48:51 +0000

    Add basic functions of loan calculator

---
+++ JS101/lesson_2_small_programs/loan-calculator.js
@@ -0,0 +1,20 @@
+const readline = require("readline-sync");
+
+let loanAmount = readline.question('How much is your loan? ');
+
+let APR = readline.question('What is your Annual Percentage Rate? ')
+
+let loanDurationMonths = readline.question('What is the duration of your loan in months? ')
+
+let monthlyInterestRate = (APR / 12) / 100
+let monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationMonths))));
+
+let totalPayment = monthlyPayment * 24
+
+let totalInterest = totalPayment - loanAmount
+
+console.log(monthlyPayment.toFixed(2))
+
+console.log(totalPayment.toFixed(2))
+
+console.log(totalInterest.toFixed(2))
