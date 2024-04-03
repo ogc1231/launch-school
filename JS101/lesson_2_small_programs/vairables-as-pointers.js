2a9003905d15aefffd674f3893b4f8119606b290
Oliver Craigie <ogc@live.ie>
2024-04-03 11:27:02 +0000

    Finsh variables as pointers exercises

---
+++ JS101/lesson_2_small_programs/vairables-as-pointers.js
@@ -0,0 +1,28 @@
+// Q1
+// Hello
+// Hello
+// both variables point to the string "Hello"
+
+// Q2
+// Goodbye
+// Hello
+// myWord is reassigned after myOtherWord has been assigned to myWord
+
+// Q3
+// ['Hi', 'Goodbye']
+// ['Hi', 'Goodbye']
+// you can mutate arrays and both reference same values
+
+// Q4
+// ['Hi', 'Bye']
+// ['Hello', 'Goodbye']
+// myWords is now referencing a new value but myOtherWords is still referencing myWords
+
+// Q5
+// ['Hi', 'Goodbye']
+// 'Hello'
+// myWords and myWord are different variables
+
+// Q6
+// ['Hi', 'Goodbye']
+// 'Hello'
