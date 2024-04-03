d5cbab8b5a5a94819a65f7abc33c56a2bb834269
Oliver Craigie <ogc@live.ie>
2024-04-03 09:23:03 +0000

    Finish scope exercises

---
+++ JS101/lesson_2_small_programs/exercises.js
@@ -0,0 +1,25 @@
+// Q1
+// logs 10
+// myFunc() resasigns num from 5 to 10 and is called before the console log
+
+// Q2
+// myFunc() logs 5 when called and reassigns num to 10
+// console.log(num) logs 10 because it was reassined in the function
+
+// Q3
+// myFunc() is new declaration/initialization of num not a reassignment
+// console.log(num) logs 5
+
+// Q4
+// Reference error, console is trying to log num = 10 but it decalred after
+
+// Q5
+// not argumnt added to function so it won't run
+// logs 5
+
+// Q6
+// logs 3, while loop executes when num = 3
+
+// Q7
+// creates an infinite loop as num is greater then 3 and end condtion is never met
+
