c930976e09babc14ffa39ef1941cc5ad4a9b84d7
Oliver Craigie <ogc@live.ie>
2024-04-03 09:56:15 +0000

    Finish objects-primitives exercises

---
+++ JS101/lesson_2_small_programs/object-primitive-exercises.js
@@ -0,0 +1,25 @@
+// Q1
+// Hello, as strings are primitve and immutable
+// the concanatition resut is a new string
+
+// Q2
+// Hello
+// Hello
+// Hello
+// strings are primitve and immutable, changes must assigned to a new variable
+
+// Q3
+// ['Hello', 'Goodbye']
+// arrays are objects and are mutable and so can be changed
+
+// Q4
+// ['Hello']
+// .concat method returns a new array
+
+// Q5
+// ['Hello']
+// is being called on the string and strings are immutable
+
+// Q6
+// ['HELLO']
+// index 0 in array is being assigned to a new string
