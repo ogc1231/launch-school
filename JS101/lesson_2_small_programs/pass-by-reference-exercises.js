d3f5e0b7438738b1bd4308ec0c1b5661a8e26352
Oliver Craigie <ogc@live.ie>
2024-04-04 17:03:52 +0000

    Finish pass by reference-value exercises

---
+++ JS101/lesson_2_small_programs/pass-by-reference-exercises.js
@@ -0,0 +1,26 @@
+// Q1
+// Hello
+// Hello
+// word not returned in function, pass-by-value
+
+// Q2
+// Hello
+// HELLO
+// pass-by-value
+
+// Q3
+// Hello
+// Hello
+// HELLO
+// pass-by-value
+
+// Q4
+// ['Hello', 'Goodbye']
+// ['Hi', 'Goodbye']
+// ['Hello', 'Goodbye'] is log before word[0] is refined to 'Hi'
+// pass-by-reference
+
+// Q5
+// ['Hello', 'Goodbye']
+// ['Hello', 'Goodbye']
+// words is no returned in function or logged
