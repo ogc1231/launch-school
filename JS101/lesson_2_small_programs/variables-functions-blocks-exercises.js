eff3429332dd8146d9bb41f1d18914f47911426a
Oliver Craigie <ogc@live.ie>
2024-04-05 09:46:49 +0000

    Finish variables, functions, blocks exercises

---
+++ JS101/lesson_2_small_programs/variables-functions-blocks-exercises.js
@@ -0,0 +1,33 @@
+// Q1
+// ["red", "green", "blue", "yellow"]
+// colors pass as argument and .push add "yellow to colors"
+
+// Q2
+// ["red", "green", "blue", undefined]
+//  no second argument in function call so undefined is psuhed to the array
+
+// Q3
+// ["red", "green", "blue", "purple, "pink]
+// purple is pushed to array first and then pink 
+
+// Q4
+// ["red", "green", "blue", "purple, "pink]
+// newColors = ["red", "green", "blue", "purple]
+// on second function call pink is pushed to newColors and returned
+// colors and newColors reference same array
+
+// Q5
+// ["red", "green", "purple"]
+// removeColor() reassigns color = 'blue'
+// addColor() adds blue back into array
+
+// Q6
+// hello!!!
+// Hello
+// hello!!!
+// arugements but no parameters in functions
+
+// Q7
+// hello
+// Hello
+// Hello!!!
