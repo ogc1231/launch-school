b00bf309a90e93fc5c99b4a1b2820e2a44170ef7
Oliver Craigie <ogc@live.ie>
2024-04-12 09:41:42 +0000

    Finish questions 1,3,4,5

---
+++ JS101/lesson_3_practice_problems/easy2.js
@@ -0,0 +1,32 @@
+// Q1
+// let advice = "Few things in life are as important as house training your important pet dinosaur.";
+// let newAdvice = advice.replace('important', 'urgent');
+// let newNewAdvice = advice.replaceAll('important', 'urgent');
+// console.log(newAdvice);
+// console.log(newNewAdvice);
+
+// Q2
+
+
+// Q3
+// let numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
+
+// let number1 = 8;  // false
+// let number2 = 95; // true
+
+// console.log(numbers.includes(number1));
+// console.log(numbers.includes(number2));
+
+// Q4
+// let famousWords = "seven years ago...";
+// let newFamousWords1 = "Four score and " + famousWords;
+// let newFamousWords2 = "Four score and ".concat(famousWords);
+// console.log(newFamousWords1);
+// console.log(newFamousWords2);
+
+// Q5
+// let array = [1, 2, 3, 4, 5];
+// array.splice(2, 1);
+// console.log(array);
+
+// Q6
