7819b4282d60f453d5907c2c68a9783f169ac558
Oliver Craigie <ogc@live.ie>
2024-04-14 10:07:56 +0000

    Fininsh easy3 questions 1-4

--- JS101/lesson_3_practice_problems/easy2.js
+++ JS101/lesson_3_practice_problems/easy2.js
@@ -89,9 +89,9 @@
 // console.log(titlePadded);
 
 // Q10
-let statement1 = "The Flintstones Rock!";
-let statement2 = "Easy come, easy go.";
+// let statement1 = "The Flintstones Rock!";
+// let statement2 = "Easy come, easy go.";
 
 
-console.log(statement1.split('').filter(letter => letter === 't').length);
-console.log(statement2.split('').filter(letter => letter === 't').length);
+// console.log(statement1.split('').filter(letter => letter === 't').length);
+// console.log(statement2.split('').filter(letter => letter === 't').length);
---
+++ JS101/lesson_3_practice_problems/easy3.js
@@ -0,0 +1,29 @@
+// Q1
+// let numbers1 = [1, 2, 3, 4];
+// let numbers2 = [1, 2, 3, 4];
+// let numbers3 = [1, 2, 3, 4];
+// let numbers4 = [1, 2, 3, 4];
+
+// numbers1 = [];
+// numbers2.length = 0;
+// numbers3.splice(0, numbers3.length);
+// while (numbers4.length) {
+//   numbers4.pop();
+// }
+
+// console.log(numbers1);
+// console.log(numbers2);
+// console.log(numbers3);
+// console.log(numbers4);
+
+// Q2
+// 1, 2, 34, 5
+// in JS array are coverted to string with +
+
+// Q3
+// hello there
+// str1 is not reassigned
+
+// Q4
+// [{ first: 42 }, { second: "value2" }, 3, 4, 5]
+
