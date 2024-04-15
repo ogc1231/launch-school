fefcd7016f432add817e4d2f38bb823da0f859b1
Oliver Craigie <ogc@live.ie>
2024-04-15 10:27:07 +0000

    Complete medium1 questions 1 and 2

---
+++ JS101/lesson_3_practice_problems/medium1.js
@@ -0,0 +1,26 @@
+// Q1
+// for (let i = 0; i <= 10; i += 1) {
+//   console.log(" ".repeat(i) + "The Flintstones Rock!")
+// }
+
+// Q2
+// let munstersDescription = "The Munsters are creepy and spooky.";
+// let munstersArray = munstersDescription.toUpperCase().split(" ")
+// munstersArray[0] = 'tHE'
+// munstersArray[1] = 'mUNSTERS'
+// let munstersDescriptionNew = munstersArray.join(' ')
+
+// console.log(munstersDescriptionNew)
+
+// let munstersDescription2 = "The Munsters are creepy and spooky.";
+
+// console.log(
+//   munstersDescription2
+//     .split("")
+//     .map(function (char) {
+//       return char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
+//     })
+//     .join("")
+// )
+
+// Q3
