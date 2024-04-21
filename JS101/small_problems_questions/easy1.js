93a99db4b7bf4fc1260f80879b87a5eb1825d6b6
Oliver Craigie <ogc@live.ie>
2024-04-21 16:06:51 +0000

    Finish question 2 and bonus

--- JS101/small_problems_questions/easy1.js
+++ JS101/small_problems_questions/easy1.js
@@ -1,11 +1,22 @@
 // Q1
-function isOdd(number) {
-    return Math.abs(number) % 2 === 1
-}
+// function isOdd(number) {
+//     return Math.abs(number) % 2 === 1
+// }
 
-console.log(isOdd(2));
-console.log(isOdd(5));
-console.log(isOdd(-17));
-console.log(isOdd(-8));
-console.log(isOdd(0));
-console.log(isOdd(7));
+// console.log(isOdd(2));
+// console.log(isOdd(5));
+// console.log(isOdd(-17));
+// console.log(isOdd(-8));
+// console.log(isOdd(0));
+// console.log(isOdd(7));
+
+// Q2
+
+// for (let i = 1; i < 100; i += 2) {
+//     console.log(i)
+// }
+
+let j = -1
+while (j < 99) {
+    console.log(j += 2)
+}
