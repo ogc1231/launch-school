46da8e3884b879f73d9a37f0bd6cd31183ecfd43
Oliver Craigie <ogc@live.ie>
2024-04-22 10:56:16 +0000

    Finish questions 3 and 4

--- JS101/small_problems_questions/easy1.js
+++ JS101/small_problems_questions/easy1.js
@@ -16,7 +16,44 @@
 //     console.log(i)
 // }
 
-let j = -1
-while (j < 99) {
-    console.log(j += 2)
-}
+// for (let i = 1; i < 100; i += 1) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// Q3
+// for (let i = 1; i < 100; i += 1) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// Q4
+// const readline = require('readline-sync');
+// const SQUARE_METER_IN_FEET = 10.7639
+
+// let operation = readline.question("Are your measuremnents in meters or Feet?\nMeters or Feet ");
+
+// while (!['Meters'.toLocaleLowerCase(), 'Feet'.toLowerCase()].includes(operation)) {
+//   console.log('Must choose Meters or Feet');
+//   operation = readline.question();
+// }
+
+// let length = readline.question(`Enter the length of the room in ${operation}: `);
+// length = parseInt(length);
+
+// let width = readline.question(`Enter the width of the room  ${operation}: `);
+// width = parseInt(width);
+
+// let areaMeters = length * width
+// let areaFeet = (areaMeters) * SQUARE_METER_IN_FEET
+
+// if (operation === 'Meters'.toLowerCase()) {
+//     console.log(`The area of the room is ${areaMeters.toFixed(2)} square meters or ${areaFeet.toFixed(2)} square feet`)
+// } else {
+//     console.log(`The area of the room is ${areaFeet.toFixed(2)} square feet or ${areaMeters.toFixed(2)} square meters`)
+// }
