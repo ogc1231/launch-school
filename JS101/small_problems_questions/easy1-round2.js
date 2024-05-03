d013615f7c7862219badf40e9f0ae60db62bd279
Oliver Craigie <ogc@live.ie>
2024-05-03 12:04:13 +0000

    Complete easy1 questions 5-11

--- JS101/small_problems_questions/easy1-round2.js
+++ JS101/small_problems_questions/easy1-round2.js
@@ -83,35 +83,189 @@
 
 // Q5
 // ----------------------------------------------------------------------------------------------------------------------------
+// const readline = require("readline-sync");
 
+// let bill = Number(readline.question('What is the bill?\n'));
+// let tipPercentage = Number(readline.question('What is the tip percentage?\n'));
+
+// let tip = bill * (tipPercentage / 100);
+// let total = bill + tip;
+
+// console.log(`The tip is $${tip.toFixed(2)}`);
+// console.log(`The total is $${total.toFixed(2)}`);
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q6
 // ----------------------------------------------------------------------------------------------------------------------------
+// const readline = require("readline-sync");
 
+// let integer = Number(readline.question('Please enter an integer greater than 0:\n'));
+
+// while (integer === 0 || Number.isNaN(integer)) {
+//     integer = Number(readline.question('Please enter an integer greater than 0:\n'));
+// } 
+
+// let choice = readline.question('Enter "s" to compute the sum, or "p" to compute the product.\n');
+
+// while (!['S'.toLowerCase(), 'P'.toLowerCase()].includes(choice)) {
+//     choice = readline.question('Must enter "s" to compute the sum, or "p" to compute the product.\n');
+// }
+
+// function sum(number) {
+//     let sum = 0;
+    
+//     for (let i = 1; i <= number; i += 1) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// function product(number) {
+//     let product = 1;
+    
+//     for (let i = 1; i <= number; i += 1) {
+//         product *= i;
+//     }
+//     return product;
+// }
+
+// if (choice === "S".toLowerCase()) {
+//     console.log(sum(integer));
+// } else if (choice === 'P'.toLowerCase()) {
+//     console.log(product(integer));
+// }
+
+// function sumArray(array) {
+//     let value = 0;
+//     let finalValue = array.reduce((accumulator, currentValue) => accumulator + currentValue, value);
+//     console.log(finalValue);
+// }
+
+// function productArray(array) {
+//     let value = 1;
+//     let finalValue = array.reduce((accumlator, currentValue) => accumlator * currentValue, value);
+//     console.log(finalValue);
+// }
+
+// let array1 = [1, 2, 3, 4, 5];
+// let array2 = [1, 2, 3, 4, 5, 6];
+
+// sumArray(array1);
+// productArray(array2);
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q7
 // ----------------------------------------------------------------------------------------------------------------------------
+// function shortLongShort(str1, str2) {
+//     if (str1.length < str2.length) {
+//         console.log(str1 + str2 + str1);
+//     } else if (str1.length > str2.length) {
+//         console.log(str2 + str1 + str2)
+//     }
+// }
 
+// shortLongShort('abc', 'defgh');    // "abcdefghabc"
+// shortLongShort('abcde', 'fgh');    // "fghabcdefgh"
+// shortLongShort('', 'xyz');         // "xyz"
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q8
 // ----------------------------------------------------------------------------------------------------------------------------
+// function isLeapYear(year) {
+//     if (year <= 0) return `ERROR - year must be greater than zero!`;
 
+//   if (year % 400 === 0) {
+//     return true;
+//   } else if (year % 100 === 0) {
+//     return false;
+//   } else {
+//     return year % 4 === 0;
+//   }
+// }
+
+// console.log(isLeapYear(2016));      // true
+// console.log(isLeapYear(2015));      // false
+// console.log(isLeapYear(2100));      // false
+// console.log(isLeapYear(2400));      // true
+// console.log(isLeapYear(240000));    // true
+// console.log(isLeapYear(240001));    // false
+// console.log(isLeapYear(2000));      // true
+// console.log(isLeapYear(1900));      // false
+// console.log(isLeapYear(1752));      // true
+// console.log(isLeapYear(1700));      // false
+// console.log(isLeapYear(1));         // false
+// console.log(isLeapYear(100));       // false
+// console.log(isLeapYear(400));       // true
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q9
 // ----------------------------------------------------------------------------------------------------------------------------
+// function isLeapYear(year) {
+//     if (year <= 0) return `ERROR - year must be greater than zero!`;
 
+//     if (year < 1752 % year % 4 === 0) {
+//         return true;  
+//     } else if (year % 400 === 0) {
+//         return true;
+//     } else if (year % 100 === 0) {
+//         return false;
+//     } else {
+//         return year % 4 === 0;
+//     }
+// }
+
+// console.log(isLeapYear(2016));      // true
+// console.log(isLeapYear(2015));      // false
+// console.log(isLeapYear(2100));      // false
+// console.log(isLeapYear(2400));      // true
+// console.log(isLeapYear(240000));    // true
+// console.log(isLeapYear(240001));    // false
+// console.log(isLeapYear(2000));      // true
+// console.log(isLeapYear(1900));      // false
+// console.log(isLeapYear(1752));      // true
+// console.log(isLeapYear(1700));      // false
+// console.log(isLeapYear(1));         // false
+// console.log(isLeapYear(100));       // false
+// console.log(isLeapYear(400));       // true
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q10
 // ----------------------------------------------------------------------------------------------------------------------------
+// function multisum(num) {
+//     let sum = 0;
+    
+//     for (let i = 0; i <= num; i += 1) {
+//         if (i % 3 === 0 || i % 5 === 0) {
+//             sum += i;
+//         }
+//     }
+//     return sum;
+// }
 
+// console.log(multisum(3));       // 3
+// console.log(multisum(5));       // 8
+// console.log(multisum(10));      // 33
+// console.log(multisum(1000));    // 234168
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q11
 // ----------------------------------------------------------------------------------------------------------------------------
+// function utf16Value(string) {
+//     let sum = 0;
+    
+//     for (let i = 0; i < string.length; i += 1) {
+//         sum += string.charCodeAt(i);
+//     }
+//     console.log(sum);
+// }
 
+
+// utf16Value('Four score');         // 984
+// utf16Value('Launch School');      // 1251
+// utf16Value('a');                  // 97
+// utf16Value('');                   // 0
+
+// const OMEGA = "\u03A9";             // UTF-16 character 'Ω' (omega)
+// utf16Value(OMEGA);                  // 937
+// utf16Value(OMEGA + OMEGA + OMEGA);  // 2811
 // ----------------------------------------------------------------------------------------------------------------------------
