8af656aded8b233ae604052ae7e35c288f75ffbd
Oliver Craigie <ogc@live.ie>
2024-05-04 11:34:54 +0000

    Complete easy2 question 1

--- JS101/small_problems_questions/easy2-round2.js
+++ JS101/small_problems_questions/easy2-round2.js
@@ -1,6 +1,15 @@
 // Q1
 // ----------------------------------------------------------------------------------------------------------------------------
+// function greetings(arr, obj) {
+//   let name = arr.join(' ');
+  
+//   return `Hello, ${name}! Nice to have a ${obj.title} ${obj.occupation} around.`
+// }
 
+// console.log(
+//   greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
+// );
+// // logs Hello, John Q Doe! Nice to have a Master Plumber around.
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q2
