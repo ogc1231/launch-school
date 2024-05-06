cf2f7643d34f8ad5252acf228ca9ac93791ff15a
Oliver Craigie <ogc@live.ie>
2024-05-06 08:56:11 +0000

    Complete easy2 question 6

--- JS101/small_problems_questions/easy2-round2.js
+++ JS101/small_problems_questions/easy2-round2.js
@@ -75,7 +75,14 @@
 
 // Q6
 // ----------------------------------------------------------------------------------------------------------------------------
+// function penultimate(string) {
+//     let array = string.split(' ');
+    
+//     return array[array.length - 2];
+// }
 
+// console.log(penultimate("last word") === "last"); // logs true
+// console.log(penultimate("Launch School is great!") === "is"); // logs true
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q7
