b9b528c0ebcb4fc6bb26989b3a0d97d0a77fbbae
Oliver Craigie <ogc@live.ie>
2024-05-09 10:27:33 +0000

    Complete easy3 questions 6-10

--- JS101/small_problems_questions/easy3-round2.js
+++ JS101/small_problems_questions/easy3-round2.js
@@ -70,25 +70,87 @@
 
 // Q6
 // ----------------------------------------------------------------------------------------------------------------------------
+// const readline = require("readline-sync");
 
+// let noun = readline.question('Enter a noun:\n');
+// let verb = readline.question('Enter a verb:\n');
+// let adjective = readline.question('Enter a adjective:\n');
+// let adverb = readline.question('Enter a adverb:\n');
+
+// console.log(`Do you ${verb} your ${adjective} ${noun} ${adverb}? That's hilarious!`);
+// console.log(`The ${adjective} ${noun} ${verb}s ${adverb} over the lazy ${noun}.`);
+// console.log(`The ${noun} ${adverb} ${verb}s up ${adjective} Joe's turtle.`);
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q7
 // ----------------------------------------------------------------------------------------------------------------------------
+// function twice(num) {
+//     let numString = String(num);
+//     let center = Math.floor(numString.length / 2);
+//     let left = numString.substring(0, center);
+//     let right = numString.substring(center);
+    
+//     if (left === right) {
+//         console.log(num);
+//     } else {
+//         console.log(num * 2);
+//     }
+// }
 
+// twice(37);          // 74
+// twice(44);          // 44
+// twice(334433);      // 668866
+// twice(444);         // 888
+// twice(107);         // 214
+// twice(103103);      // 103103
+// twice(3333);        // 3333
+// twice(7676);        // 7676
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q8
 // ----------------------------------------------------------------------------------------------------------------------------
+// function getGrade(grade1, grade2, grade3) {
+//     let average = (grade1 + grade2 + grade3) / 3;
+    
+//     if (average >= 90 && average < 101) {
+//         console.log('A');
+//     } else if (average >= 80) {
+//         console.log('B');
+//     } else if (average >= 70) {
+//         console.log('C');
+//     } else if (average >= 60) {
+//         console.log('D');
+//     } else {
+//         console.log('F');
+//     }
+// }
 
+
+// getGrade(95, 90, 93);    // "A"
+// getGrade(50, 50, 95);    // "D"
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q9
 // ----------------------------------------------------------------------------------------------------------------------------
+// function cleanUp(string) {
+//     console.log(string.replace(/[^a-z]+/gi, ' '));
+// }
 
+// cleanUp("---what's my +*& line?");    // " what s my line "
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q10
 // ----------------------------------------------------------------------------------------------------------------------------
+function century(year) {
+}
 
+console.log(century(2000));        // "20th"
+console.log(century(2001));        // "21st"
+console.log(century(1965));        // "20th"
+console.log(century(256));         // "3rd"
+console.log(century(5));           // "1st"
+console.log(century(10103));       // "102nd"
+console.log(century(1052));        // "11th"
+console.log(century(1127));        // "12th"
+console.log(century(11201));       // "113th"
 // ----------------------------------------------------------------------------------------------------------------------------
