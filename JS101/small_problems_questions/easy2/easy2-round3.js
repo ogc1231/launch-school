068a4ccef5d9ec003af896da122793b6c16e1e43
Oliver Craigie <ogc@live.ie>
2024-05-10 12:29:03 +0000

    Finish easy2 questions 7

--- JS101/small_problems_questions/easy2/easy2-round3.js
+++ JS101/small_problems_questions/easy2/easy2-round3.js
@@ -109,7 +109,20 @@
 
 // Q7
 // ----------------------------------------------------------------------------------------------------------------------------
+// function xor(num1, num2) {
+//   if ((num1 && !num2) || (num2 && !num1)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
+// console.log(xor(5, 0) === true);          // true
+// console.log(xor(false, true) === true);   // true
+// console.log(xor(1, 1) === false);         // true
+// console.log(xor(true, true) === false);   // true
+// console.log(xor(0, false) === false);     // true
+// console.log(xor(0, 0) === false);         // true
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // Q8
