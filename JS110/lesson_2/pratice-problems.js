9cf0cf81957b35889890531be118a3c88f4f4110
Oliver Craigie <ogc@live.ie>
2024-07-29 11:46:28 +0000

    Practice problems 9-17

--- JS110/lesson_2/pratice-problems.js
+++ JS110/lesson_2/pratice-problems.js
@@ -1,8 +1,8 @@
-// // 1
+// ---------------------------------------- 1
 // let arr = ['10', '11', '9', '7', '8'];
 // console.log(arr.sort((a, b) => b - a));
 
-// // 2
+// ---------------------------------------- 2
 // let books = [
 //   { title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967' },
 //   { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925' },
@@ -15,7 +15,7 @@
 //   return Number(a.published) - Number(b.published);
 // }));
 
-// // 3
+// ---------------------------------------- 3
 // let arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]];
 // console.log(arr1[2][1][3]);
 
@@ -31,7 +31,7 @@
 // let obj2 = { first: { d: 3 }, second: { e: 2, f: 1 }, third: { g: 0 }};
 // console.log(Object.keys(obj2.third)[0]);
 
-// // 4
+// ---------------------------------------- 4
 // let arr1 = [1, [2, 3], 4];
 // arr1[1][1] = 4;
 // console.log(arr1);
@@ -48,7 +48,7 @@
 // obj2.a.a[2] = 4;
 // console.log(obj2);
 
-// // 5
+// ---------------------------------------- 5
 // let munsters = {
 //   Herman: { age: 32, gender: 'male' },
 //   Lily: { age: 30, gender: 'female' },
@@ -80,7 +80,7 @@
 
 // console.log(totalMaleAge);
 
-// // 6
+// ---------------------------------------- 6
 // let munsters = {
 //   herman: { age: 32, gender: 'male' },
 //   lily: { age: 30, gender: 'female' },
@@ -99,7 +99,7 @@
 //   console.log(`${name} is a ${age}-year-old ${gender}.`)
 // });
 
-// // 7
+// ---------------------------------------- 7
 // let a = 2;
 // let b = [5, 8];
 // let arr = [a, b]; // [2, [5, 8]]
@@ -111,7 +111,7 @@
 // console.log(b); // => [3, 8]
 // console.log(arr); // => [4, [3, 8]];
 
-// // 8
+// ---------------------------------------- 8
 // let obj = {
 //   first: ['the', 'quick'],
 //   second: ['brown', 'fox'],
@@ -132,3 +132,130 @@
 //     });
 //   });
 // });
+
+// ---------------------------------------- 9
+// let arr = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']];
+
+// console.log(arr.map(subArr => {
+//     if (typeof subArr[0] === 'string') {
+//         return subArr.slice().sort();
+//     } else {
+//         return subArr.slice().sort((a, b) => a - b);
+//     }
+// }));
+
+// ---------------------------------------- 10
+// let arr = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']];
+
+// console.log(arr.map(subArr => {
+//     if (typeof subArr[0] === 'string') {
+//         return subArr.slice().sort().reverse();
+//     } else {
+//         return subArr.slice().sort((a, b) => b - a);
+//     }
+// }));
+
+// ---------------------------------------- 11
+// let arr = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }];
+
+// console.log(arr.map(obj => {
+//     let newObject = {};
+    
+//     for (let key in obj) {
+//         newObject[key] = obj[key] + 1;
+//     }
+    
+//     return newObject;
+// }));
+
+// console.log(arr);
+
+// ---------------------------------------- 12
+// const arr = [
+//   ['b', 'c', 'a'],
+//   ['blue', 'black', 'green'],
+//   { b: [2, 4, 6], c: [3, 6], d: [4] },
+//   { e: [8], f: [6, 10] },
+// ];
+
+// let deepArr = JSON.parse(JSON.stringify(arr));
+
+// deepArr.push('foo')
+// deepArr[0].push('w')
+// deepArr[2]['d'].push(25)
+
+// console.log(arr);
+// console.log(deepArr);
+
+// ---------------------------------------- 13
+// const truthiness = {
+//   falsy: [ null, undefined, '', false, NaN, 0 ],
+//   truthy: ['everything else...']
+// };
+
+// const deepCopy = {};
+
+// for (let prop in truthiness) {
+//     deepCopy[prop] = [...truthiness[prop]]
+// }
+
+// console.log(truthiness);
+// console.log(deepCopy);
+
+// ---------------------------------------- 14
+// let arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
+
+// console.log(arr.map(subArr => {
+//     return subArr.filter(num => num % 3 === 0);
+// }));
+
+// ---------------------------------------- 15
+// let arr = [[1, 6, 7], [1, 5, 3], [1, 8, 3]];
+
+// console.log(arr.sort((a, b) => {
+//     let sumA = a.filter(num => num % 2 === 1)
+//                 .reduce((sum, current) => sum + current);
+//     let sumB = b.filter(num => num % 2 === 1)
+//                 .reduce((sum, current) => sum + current);
+                
+//     return sumA - sumB;
+// }));
+
+// ---------------------------------------- 16
+// let obj = {
+//   grape: { type: 'fruit', colors: ['red', 'green'], size: 'small' },
+//   carrot: { type: 'vegetable', colors: ['orange'], size: 'medium' },
+//   apple: { type: 'fruit', colors: ['red', 'green'], size: 'medium' },
+//   apricot: { type: 'fruit', colors: ['orange'], size: 'medium' },
+//   marrow: { type: 'vegetable', colors: ['green'], size: 'large' },
+// };
+
+// let values = Object.values(obj);
+
+// console.log(values.map(attributes => {
+//     if (attributes['type'] === 'fruit') {
+//         return attributes['colors'].map(word => word[0].toUpperCase() + word.slice(1));
+//     } else {
+//         return attributes['size'].toUpperCase();
+//     }
+// }));
+
+// ---------------------------------------- 17
+// let arr = [
+//   { a: [1, 2, 3] },
+//   { b: [2, 4, 6], c: [3, 6], d: [4] },
+//   { e: [8], f: [6, 10] },
+// ];
+
+// console.log(arr.filter(obj => {
+//     return Object.values(obj).every(subArr => {
+//         return subArr.every(num => num % 2 === 0);
+//     });
+// }));
+// ---------------------------------------- 18
+
+// ---------------------------------------- 19
+
+// ---------------------------------------- 20
+
+// ---------------------------------------- 21
